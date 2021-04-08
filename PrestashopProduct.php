61402907ca7e2a1837964ab3bb09fdc0ddbc54e3
Rindra Raininoro <rraininoro@gmail.com>
2021-04-08 20:28:27 +0400

    Add files via upload

---
+++ PrestashopProduct.php
@@ -0,0 +1,111 @@
+<?php
+
+
+namespace Components\PrestashopApiClient;
+
+class PrestashopProduct
+{
+    /**
+     * @var int
+     */
+    private $id;
+    /**
+     * @var array
+     */
+    private $descriptions;
+    /**
+     * @var array
+     */
+    private $shortDescriptions;
+    /**
+     * @var array
+     */
+    private $metaDescriptions;
+    /**
+     * @var array
+     */
+    private $metaTitles;
+    /**
+     * @var array
+     */
+    private $names;
+    /**
+     * @var array
+     */
+    private $linkRewrites;
+
+    /**
+     * @return array
+     */
+    public function getDescriptions(): array
+    {
+        return $this->descriptions;
+    }
+
+    /**
+     * @return array
+     */
+    public function getShortDescriptions(): array
+    {
+        return $this->shortDescriptions;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMetaDescriptions(): array
+    {
+        return $this->metaDescriptions;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMetaTitles(): array
+    {
+        return $this->metaTitles;
+    }
+
+    /**
+     * @return array
+     */
+    public function getNames(): array
+    {
+        return $this->names;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLinkRewrites(): array
+    {
+        return $this->linkRewrites;
+    }
+
+    public function __construct(string $apiKey, int $id)
+    {
+        $this->init($apiKey, $id);
+    }
+
+
+    private function init(string $apiKey, int $id)
+    {
+        $result = file_get_contents("https://{$apiKey}@dev.artemi.be/api/products/{$id}?output_format=JSON");
+        $result = json_decode_utf8($result);
+        $this->id = $result->product->id;
+        $this->descriptions = $result->product->description;
+        $this->metaDescriptions = $result->product->meta_description;
+        $this->metaTitles = $result->product->meta_title;
+        $this->names = $result->product->name;
+        $this->shortDescriptions = $result->product->description_short;
+        $this->linkRewrites = $result->product->link_rewrite;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
---
+++ PrestashopProductApiCLient.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Components\PrestashopApiClient;
+
+class PrestashopProductApiCLient
+{
+    /**
+     * @var array
+     */
+    private $productIds = [];
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    public function __construct(string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+        $json = file_get_contents("https://{$apiKey}@dev.artemi.be/api/products/?output_format=JSON");
+        $this->setProductIds($json);
+    }
+
+    /**
+     * @param string $json
+     */
+    private function setProductIds(string $json)
+    {
+        $this->productIds = [];
+        $objIds = json_decode_utf8($json);
+        $objIds = $objIds->products;
+        foreach ($objIds as $objId) {
+            array_push($this->productIds, $objId->id);
+        }
+    }
+
+    /**
+     * @param int $limit
+     * @return array
+     */
+    public function getProducts(int $limit)
+    {
+        $prestashopProducts = [];
+        foreach ($this->productIds as $key => $productId) {
+            if ($key == $limit) {
+                break;
+            }
+            array_push($prestashopProducts, new PrestashopProduct($this->apiKey, $productId));
+        }
+        return $prestashopProducts;
+    }
+}
